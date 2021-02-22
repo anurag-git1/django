28c72b6274a346df880eb7f1c41badc864df76f2
Anurag nadda <anuragn@squadmindsinc.com>
2021-02-22 11:53:20 +0530

    comment

--- ItsAJoke/settings.py
+++ ItsAJoke/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'mzakapp',
     'rest_framework',
 ]
-
+# use between routes and views
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
